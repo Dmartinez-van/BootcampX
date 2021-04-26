cec6fd92b832e45840b3ca3783a601eeadccefbb
qPasta <67769992+Dmartinez-van@users.noreply.github.com>
2021-04-26 14:07:18 -0700

    Create assignments_submissions.sql table creation file (Fix to spelling)

--- migrations/assignments_submissions.sql
+++ migrations/assignments_submissions.sql
@@ -1,4 +1,4 @@
-CREATE TEABLE assignments (
+CREATE TABLE assignments (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255),
   content TEXT,
@@ -7,7 +7,7 @@ CREATE TEABLE assignments (
   duration INTEGER
 );
 
-CREATE TEABLE assignment_submissions (
+CREATE TABLE assignment_submissions (
   id SERIAL PRIMARY KEY NOT NULL,
   assignment_id INTEGER REFERENCES assignments(id) ON DELETE CASCADE,
   student_id INTEGER INTEGER REFERENCES students(id) ON DELETE CASCADE,
