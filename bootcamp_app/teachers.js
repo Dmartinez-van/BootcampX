7ab386f51d9ab85bcfe518b1c556fcce07df45c4
qPasta <67769992+Dmartinez-van@users.noreply.github.com>
2021-04-28 19:13:40 -0700

    Update to parameterized queries

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -9,17 +9,17 @@ const pool = new Pool({
 
 const cohort = process.argv[2];
 // const limit = process.argv[3] || 5;
-console.log('connected...');
-pool.query(
-  `
+const queryString = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name LIKE '%${cohort}%'
+WHERE cohorts.name LIKE $1
 ORDER BY teacher;
-`)
+`;
+console.log('connected...');
+pool.query(queryString, [`%${cohort}%`])
   .then(res => {
     res.rows.forEach(teacher => {
       // console.log(teacher);
